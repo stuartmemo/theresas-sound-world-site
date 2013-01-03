977359f1c039a2ff27bd9e5e3c9ebd6d680cc03b
Stuart Memo <stuartmemo@gmail.com>
2013-01-03 00:42:37 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , fs = require('fs')
+  , hbs = require('hbs')
+  , mongoose = require('mongoose');
+
+var app = express();
+hbs.registerPartial('header', fs.readFileSync(__dirname + '/views/header.hbs', 'utf8'));
+hbs.registerPartial('menu', fs.readFileSync(__dirname + '/views/menu.hbs', 'utf8'));
+hbs.registerPartial('footer', fs.readFileSync(__dirname + '/views/footer.hbs', 'utf8'));
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'hbs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+mongoose.connect('mongodb://localhost/theresaSite');
+
+var CommandSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    example: String
+});
+
+var Commands = mongoose.model('Commands', CommandSchema);
+
+app.get('/', function (req, res) {
+    res.render('index.hbs', {title: 'home page'})
+});
+
+app.get('/reference', function (req, res) {
+    Commands.find({}, function (err, docs) {
+        res.render('reference.hbs', {commands: docs});
+    });
+});
+
+app.get('/reference/:command', function (req, res) {
+    res.send('Hi ' + req.params.command)
+})
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
