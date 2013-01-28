afb4bbb9a894c0a6c311bb2761f9734d477f1d70
Stuart Memo <stuartmemo@gmail.com>
2013-01-28 00:28:33 +0000

    added effects example page

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-  , fs = require('fs')
-  , hbs = require('hbs')
-  , mongoose = require('mongoose');
+var express = require('express'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path'),
+    fs = require('fs'),
+    hbs = require('hbs'),
+    mongoose = require('mongoose');
 
 var app = express();
 hbs.registerPartial('header', fs.readFileSync(__dirname + '/views/header.hbs', 'utf8'));
@@ -17,19 +17,19 @@ hbs.registerPartial('menu', fs.readFileSync(__dirname + '/views/menu.hbs', 'utf8
 hbs.registerPartial('footer', fs.readFileSync(__dirname + '/views/footer.hbs', 'utf8'));
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'hbs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'hbs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 });
 
 mongoose.connect('mongodb://localhost/theresaSite');
@@ -73,6 +73,12 @@ app.get('/reference/:command', function (req, res) {
 app.get('/examples', function (req, res) {
     Commands.find({}, function (err, docs) {
         res.render('examples.hbs');
+    });
+});
+
+app.get('/examples/effects', function (req, res) {
+    Commands.find({}, function (err, docs) {
+        res.render('effects.hbs');
     });
 });
 
