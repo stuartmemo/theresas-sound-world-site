7c7c742896f1d772d4c87219ced5418f45e8217c
Stuart Memo <stuartmemo@gmail.com>
2014-01-13 15:27:34 +0000

    improved Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function (grunt) {
-    'use strict';
-
+    'use strict'; 
     // Project config.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -8,7 +7,7 @@ module.exports = function (grunt) {
         uglify: {
             all: {
                 files: {
-                    'build/tsw.min.js': ['src/tsw-core.js', 'src/tsw-effects.js', 'src/tsw-music.js']
+                    'build/tsw.min.js': ['src/scripts/tsw-core.js', 'src/scripts/tsw-effects.js', 'src/scripts/tsw-music.js']
                 },
                 options: {
                     banner: '/* <%= pkg.title %> <%= pkg.version %> (c) 2013 Stuart Memo */\n'
@@ -16,19 +15,43 @@ module.exports = function (grunt) {
             }
         },
 
-        concat: {
-            options: {
-                banner: '/* <%= pkg.title %> <%= pkg.version %> (c) 2013 Stuart Memo */\n'
-            },
-            dist: {
-                src: ['']
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        src: 'src/index.html',
+                        dest: 'build/',
+                        flatten: true,
+                        filter: 'isFile'
+                    }
+                ]
+            }
+        },
+
+        watch: {
+            html: {
+                files: 'src/index.html',
+                tasks: ['uglify', 'copy']
+            }
+        },
+        
+        connect: {
+            server: {
+                options: {
+                    port: 9000,
+                    keepalive: true
+                }
             }
         }
     });
 
     // Load plugins.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default tasks.
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['watch', 'connect']);
 };
